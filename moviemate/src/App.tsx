f8eb67bac40bf3b49ee8ebfd94eb34bf8c9b5c7e
Tobias Liene Borning <tobiaslb@stud.ntnu.no>
2024-02-08 14:43:15 +0100

    add BasicComponent to App

--- moviemate/src/App.tsx
+++ moviemate/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import BasicComponent from './components/basicComponent';
 import TextLayout from './components/textLayout';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <TextLayout />
+      <BasicComponent />
     </div>
   );
 }
