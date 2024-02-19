327753ed4075241305c2e7d30bd6c62db4613ac6
Tobias Liene Borning <tobiaslb@stud.ntnu.no>
2024-02-19 09:21:15 +0100

    [Add] started making on navbar in main page

---
+++ moviemate/src/components/Navbar/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const NavBar: React.FC = () => {
+    return (
+        <nav>
+        <h1>Moviemate</h1>
+        <div>
+            <button>Log in</button>
+            <button>Sign up</button>
+        </div>
+        </nav>
+    );
+    }
