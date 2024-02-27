18534e34d4dfd6744cefe294c1bb00e83d4307da
abdihake@stud.ntnu.no <abdihake@stud.ntnu.no>
2024-02-27 11:59:03 +0100

    Fixes.

--- moviemate/src/pages/Main/mainPage.tsx
+++ moviemate/src/pages/Main/mainPage.tsx
@@ -1,10 +1,17 @@
 import ScrollingComponent from "../../components/Scrolling/ScrollingComponent";
-
+import { useNavigate } from "react-router";
+import Profile from "../Profile/Profile";
 import React from 'react';
 
 const MainPage: React.FC = () => {
+    const navigate = useNavigate();
     return (
         <div>
+            
+            <button onClick={() => navigate('/profile')}>
+          Profile
+            </button>
+        
             <ScrollingComponent />
         </div>
     );
---
+++ moviemate/src/pages/Profile/Profile.tsx
@@ -0,0 +1,15 @@
+import ScrollingComponent from "../../components/Scrolling/ScrollingComponent";
+
+import React from 'react';
+
+const Profile: React.FC = () => {
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+    );
+}
+
+export default Profile;
+
+
