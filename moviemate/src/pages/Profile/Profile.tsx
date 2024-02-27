3753d0d2feaf869d7108da6f7d9a12a35447eb51
abdihake@stud.ntnu.no <abdihake@stud.ntnu.no>
2024-02-27 14:14:52 +0100

    Added outline for Profile Card, and Profile Pictures

--- moviemate/src/pages/Profile/Profile.tsx
+++ moviemate/src/pages/Profile/Profile.tsx
@@ -1,11 +1,11 @@
-import ScrollingComponent from "../../components/Scrolling/ScrollingComponent";
-
 import React from 'react';
+import Profile_Card from './Profile_Card/Profile_Card';
 
 const Profile: React.FC = () => {
     return (
         <div>
             <h1>Test</h1>
+           < Profile_Card />
         </div>
     );
 }
