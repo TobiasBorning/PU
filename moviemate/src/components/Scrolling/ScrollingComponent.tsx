c992dd8597ac78e22fb521cb8e0b348217e8ae83
Tobias Liene Borning <tobiaslb@stud.ntnu.no>
2024-02-28 20:42:38 +0100

    [Fix] Main page now loads 20 movies at a time
    * Changed utility function
    * Added button to load more movies. Bug: needs two clicks to load first time

--- moviemate/src/components/Scrolling/ScrollingComponent.tsx
+++ moviemate/src/components/Scrolling/ScrollingComponent.tsx
@@ -2,63 +2,106 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './ScrollingComponent.css';
 import { getMovies, Movie } from '../../utils/movieUtils/fetchAndFillDb';
-import { NavBar } from '../Navbar/NavBar';
+//import { NavBar } from '../Navbar/NavBar';
 import { User, getUser } from '../../utils/login/users';
 import { auth } from '../../config/firebase';
 
 function ScrollingComponent() {
     const [user, setUser] = useState<string>('');
     const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState<boolean>(true);
     const [movieList, setMovieList] = useState<Movie[]>([]);
+    const [boxArray,setBoxArray] = useState<JSX.Element[]>([]);
+    const [movieCount, setMovieCount] = useState<number>(20);
 
+
+    // henter filmer fra databasen
     useEffect(() => {
         const fillMovieList = async () => {
-            getMovies().then((movies) => {
-                setMovieList(movies);
-            });
+            if (movieList.length === 0) {
+                getMovies(movieCount).then((movies) => {
+                    setMovieList(movies);
+                });
+            }
         };
-        getName();
         fillMovieList();
-    }, []);
+    }, [movieCount]);
+
+    // fyller container med filmer når filmene er hentet
+    useEffect(() => {
+        fillContainer();
+        getName();
+    },[movieList]);
+
+    //laster inn siden når både bruker og movielist er fylt
+    useEffect(() => {
+        if (movieList.length > 0 && user) {
+            setIsLoading(false);
+        }
+    }, [movieList, user]);
 
     const getName = async () => {
-        const user = auth.currentUser;
-        if (user) {
-            await getUser(user.uid).then((user: User) => {
-                setUser(', ' + user.firstname);
-            });       
+        if (isLoading) {
+            const user = auth.currentUser;
+            if (user) {
+                await getUser(user.uid).then((user: User) => {
+                    setUser(', ' + user.firstname);
+                });       
+            }
         }
+        
     }
 
+    // navigerer til en filmside
     const goToMovie = (number: number) => {
         navigate('/movie', { state: { number } });
     }
     
-    const boxes = movieList.map((movie) => (
-        <div key={movie.id} className="box" onClick={() => {
-            if (movie.id !== undefined) { 
-                goToMovie(parseInt(movie.id));
-            } else {
-                console.error("Movie ID is undefined");
-            }
-        }}> 
-            <h3>{`${movie.title}`}</h3>
-            <img src={movie.posterUrl} alt="" />
-        </div>
-    ));
-    
+    // fyller container med filmer
+    const fillContainer = () => {
+        const b = movieList.map((movie) => (
+            <div key={movie.id} className="box" onClick={() => {
+                if (movie.id !== undefined) { 
+                    goToMovie(parseInt(movie.id));
+                } else {
+                    console.error("Movie ID is undefined");
+                }
+            }}> 
+                <h3>{`${movie.title}`}</h3>
+                <img src={movie.posterUrl} alt="" />
+            </div>
+        ));
+        setBoxArray(b);
+    }
+
+    // øker filmer med 20
+    // TODO: oppdaterer ikke før 2 klikk
+    const increaseMovieCount = () => {
+        setMovieCount(movieCount + 20);
+        console.log(movieCount);
+        getMovies(movieCount).then((movies) => {
+            setMovieList(movies);   
+        });
+        fillContainer();
+    }
 
     return (
         <div className='mainPageContainer'>
-            <br />
-            <div className='welcomeText'>
-                <h1>Hey{user}</h1>
-                <p>Welcome to Moviemate</p>
+            {isLoading ? <h1 className='welcomeText'>Loading...</h1>
+            :
+            <div>
+                <br />
+                <div className='welcomeText'>
+                    <h1>Hey{user}</h1>
+                    <p>Welcome to Moviemate</p>
+                </div>
+                <br />
+                <div className="scrollingContainer">
+                    {boxArray}
+                    <button onClick={increaseMovieCount}>Load more movies..</button>
+                </div>
             </div>
-            <br />
-            <div className="scrollingContainer">
-                {boxes}
-            </div>
+            }
         </div>
     );
 }
