8f51ed84e056d668ab1b16b1fbab77869e529235
Tobias Liene Borning <tobiaslb@stud.ntnu.no>
2024-03-05 11:55:51 +0100

    [Merge features] Combined backend and frontend rate and comment function
    * Users can now rate and comments movies
    
    Co-authored-by: @solvaarm

--- moviemate/src/components/Movie/ShowMovie.tsx
+++ moviemate/src/components/Movie/ShowMovie.tsx
@@ -3,18 +3,14 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import './ShowMovie.css';
 import { getMovie, Movie } from '../../utils/movieUtils/fetchAndFillDb';
 import { auth } from '../../config/firebase';
-import { Ræting, getMovieReview, Comment } from './Comment&RateMovie/Ræting';
-import { addMovieToUser, getUserMovies, isInMyMovies, removeMovieFromUser } from '../../utils/user/users';
+import { addMovieToUser, isInMyMovies, removeMovieFromUser } from '../../utils/user/users';
+import ReviewMovie from './ReviewMovie';
 
 function ShowMovie() {
     const location = useLocation();
     const movieId = location.state?.number;
     const [movie, setMovie] = useState<Movie>({
         title: 'Loading...',
-    });
-    const [review, setReview] = useState<Ræting>({
-        rating: 0,
-        comment: '',
     });
     const navigate = useNavigate();
     const [isInList,setIsInList] = useState(false);
@@ -41,25 +37,6 @@ function ShowMovie() {
             fetchMovie();
         }
     });
-
-    useEffect(() => {
-        const fetchReview = async () => {
-            if (movieId !== undefined && auth.currentUser) {
-                console.log("Fetching review");
-                const review = await getMovieReview(auth.currentUser.uid, movieId.toString());
-                console.log(review);
-                setReview(review); 
-            }
-            else {
-                console.error('Movie ID is undefined or user not logged in');
-                setReview({
-                    rating: 0,
-                    comment: '',
-                })
-            }
-        };
-        fetchReview();
-    },[auth.currentUser,movieId]);
     
     const linkUserToMovie = () => {
         const authUser = auth.currentUser;
@@ -81,6 +58,7 @@ function ShowMovie() {
         else {
             linkUserToMovie();
         }
+        setIsInList(!isInList);
     }
 
     return (
@@ -92,12 +70,9 @@ function ShowMovie() {
                 <p>Actors: {movie.actors?.join(", ")}</p>
                 <p>Director: {movie.director}</p>
                 <p>Plot: {movie.plot}</p>
-                <p>Rating: {review.rating}</p>
-                <p>Comment: {review.comment}</p>
                 <img src={movie.posterUrl} alt=''/> 
-                <Comment />
-                <img src={movie.posterUrl} alt='' />
                 <br />
+                <ReviewMovie />
                 <button onClick={() => addOrRemove()}>
                     {isInList ? 'Remove from my list' : 'Add to my list'}
                 </button>
