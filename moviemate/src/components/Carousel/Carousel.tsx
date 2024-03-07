a3998b4ef76512120088daf56c2eb72c861b524d
Anniken Strand <annikenqs@gmail.com>
2024-03-07 14:44:32 +0100

    [Merge] Merged carousel.tsx

--- moviemate/src/components/Carousel/Carousel.tsx
+++ moviemate/src/components/Carousel/Carousel.tsx
@@ -3,11 +3,10 @@ import { useNavigate } from 'react-router-dom';
 import { getMovies, Movie } from '../../utils/movieUtils/fetchAndFillDb';
 import './Carousel.css';
 import '../Scrolling/ScrollingComponent.css';
-import { getMovieByGenreOr } from '../../utils/searchUtils/searchFunctions';
-import { error } from 'console';
 import Slider from "react-slick";
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
+import { getMovieByGenreOr } from '../../utils/searchUtils/searchFunctions';
 export { };
 
 type Props = {
@@ -30,6 +29,27 @@ const Carousel: React.FC<Props> = ({ movieLimit }) => {
   const goToMovie = (number: number) => {
     navigate('/movie', { state: { number } });
   }
+
+  const selectGenre = (genre: string) => {
+    if (genre){
+      getMovieByGenreOr([genre])
+      .then((filteredMovies) => {
+        const convertedMovies: Movie[] = filteredMovies.map(movie => ({
+          ...movie,
+          posterUrl: movie.posterUrl ? String(movie.posterUrl) : undefined,
+          plot: movie.plot ? String(movie.plot) : undefined
+        }));
+        setMovieList(convertedMovies);
+        setCurrentIndex(0);
+      });
+    }
+    else {
+      getMovies(movieLimit).then((movies) => {
+        setMovieList(movies)
+        setCurrentIndex(0);
+      });
+    }
+  };
 
   const handleSlideChange = (index: number) => {
     setCurrentIndex(index);
@@ -57,29 +77,7 @@ const Carousel: React.FC<Props> = ({ movieLimit }) => {
     arrows: false
   };
 
-  const selectGenre = (genre: string) => {
-    if (genre){
-      getMovieByGenreOr([genre])
-      .then((filteredMovies) => {
-        const convertedMovies: Movie[] = filteredMovies.map(movie => ({
-          ...movie,
-          posterUrl: movie.posterUrl ? String(movie.posterUrl) : undefined,
-          plot: movie.plot ? String(movie.plot) : undefined
-        }));
-        setMovieList(convertedMovies);
-        setCurrentIndex(0);
-      });
-    }
-    else {
-      getMovies(movieLimit).then((movies) => {
-        setMovieList(movies)
-        setCurrentIndex(0);
-      });
-    }
-  };
-
   return (
-    <div className='mainPageConatiner'>
     <div className="scrolling-carousel">
       <button className="nav-button prev" onClick={handleClickPrevious} disabled={currentIndex === 0}>
         &#10094;
@@ -93,13 +91,14 @@ const Carousel: React.FC<Props> = ({ movieLimit }) => {
           </div>
         ))}
       </Slider>
-      <button onClick={() => selectGenre("Action")}>Filter Action</button>
+
       <button className="nav-button next" onClick={handleClickNext} disabled={currentIndex === movieList.length - 1}>
         &#10095;
       </button>
-    </div>
+      <button onClick={() => selectGenre("Action")}>Filter Action</button>
     </div>
   );
 }
 
 export default Carousel;
+
